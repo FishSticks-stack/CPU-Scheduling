02f9cd03bdfd8d7f38246ea2d649164619b0f3a5
vanessamartinez <spicykiwi78@gmail.com>
2022-02-24 16:19:54 -0600

    finished FCFS

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 # scheduler will be assigned a set of tasks and schedule based on the selected algorithm
-# each task has a priority and cpu burst.
 # TODO First Come First Served (FCFS)
 # schedules in the order in which they request the cpu
 # TODO Priority scheduling
@@ -11,9 +10,44 @@
 # low (1 to 10) high priority
 # TODO insert tasks into a list and invoke the scheduler
 
-# FCFS
-def fcfs():
+
+# handling the schedule.txt
+# for FCFS
+def fileS():
+    # list to hold tasks
+    items = []
+    with open("schedule.txt", 'r') as FO:
+        allLines = FO.readlines()
+    # remove invisible extra characters and commas
+    for aLine in allLines:
+        aLine = aLine.replace(',', '')
+        aLine = aLine.strip()
+        # putting tasks into a nested list to grab individual tasks and their attributes
+        items.append(aLine.split(" "))
+
+    print(f"This is the data we are working with: {items}\n")
+    return items
+    # grab T7 out of ['T7', '3', '30']
+    #print(items[0][0])
+
+def output():
+
     print("First Come First Serve Scheduling")
-    # take file that holds tasks
-    f = open("schedule.txt")
+    # taking items list from fileS method
+    name = fileS()
+    # start time
+    started = 0
+    for i in name:
+        name = i[0]
+        priority = i[1]
+        burst = i[2]
+        print(f"Started at: {started}")
+        started = started + int(burst)
+        print(f"Will run name: {name}")
+        print(f"Priority: {priority}")
+        print(f"Burst: {burst}")
+        print(f"Ended at: {started}\n")
+
+
+output()
 
